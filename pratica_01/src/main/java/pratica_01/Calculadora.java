517c83d66d168e99cf3c2c7250854eb85d55bcea
wesleyvs <wesleyvini.social@gmail.com>
2021-03-24 00:52:17 -0300

    Configuração para poder usar 'gradle run'

--- pratica_01/src/main/java/pratica_01/Calculadora.java
+++ pratica_01/src/main/java/pratica_01/Calculadora.java
@@ -2,6 +2,8 @@ package pratica_01;
 
 public class Calculadora {
 	
+	public static void main(String[] args) {}
+	
 	//Este método irá realizar o calculo da raiz cúbica utilizando a lib Math
 	public double raizCubica(double x) {
 		return Math.cbrt(x);
